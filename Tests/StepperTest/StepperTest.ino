f55926290fe49bdd2434aec75309c2b3f4f25cc7
DerFussek <tildussel@outlook.de>
2025-04-27 17:58:03 +0200

    TestSketches

---
+++ Tests/StepperTest/StepperTest.ino
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include "A4988.h"
+
+// Motor-Konfiguration
+#define MOTOR_STEPS 200       
+#define MICROSTEPS 16         
+#define RPM 120               
+
+// Pins für A4988
+#define DIR 2
+#define STEP 3
+#define SLEEP 4
+#define MS1 7
+#define MS2 6
+#define MS3 5
+
+
+A4988 stepper(MOTOR_STEPS, DIR, STEP, SLEEP, MS1, MS2, MS3);
+
+void setup() {
+  Serial.begin(115200);
+
+
+  stepper.begin(RPM, MICROSTEPS);
+
+
+  stepper.enable();
+
+
+  stepper.setSpeedProfile(stepper.LINEAR_SPEED);
+}
+
+void loop() {
+  Serial.println("Drehe nach rechts");
+  stepper.rotate(360);      
+  delay(1000);              
+
+  Serial.println("Drehe nach links");
+  stepper.rotate(-360);     
+  delay(1000);              
+}
---
+++ Tests/UltraschallSensorTest/UltraschallSensorTest.ino
@@ -0,0 +1,42 @@
+// Pin-Definition
+const int trigPin = 9;
+const int echoPin = 10;
+
+// Variable zur Entfernung
+long duration;
+float distance;
+
+void setup() {
+  // Serielle Verbindung starten
+  Serial.begin(115200);
+
+  // Pin-Modus festlegen
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  // Sicherstellen, dass Trigger LOW ist
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // 10µs HIGH-Puls senden
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Dauer des Echos messen
+  duration = pulseIn(echoPin, HIGH);
+
+  // Entfernung berechnen
+  distance = (duration * 0.0343) / 2;
+
+  // Entfernung auf Serial Monitor ausgeben
+
+  Serial.print(distance);
+  Serial.println("cm");
+
+
+  // kleine Pause
+  delay(250);
+}
